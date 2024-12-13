b281f56dd08427d35d99aa6c5f31b9ca44ec736f
viededario <darioocceus@gmail.com>
2024-12-13 15:12:55 -0500

    react-auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-// src/App.jsx
+// App.jsx
 
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm' // import the SignupForm
+import SigninForm from './components/SigninForm/SigninForm'
+import * as authService from '../src/services/authService'; // import the authservice
+
+
+
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+
+  const handleSignout = () => {
+    authService.signout()
+    setUser(null)
+  }
+  
 
   return (
     <>
-      <NavBar user={user} />
-      <h1>Hello world!</h1>
+    <NavBar user={user} handleSignout={handleSignout} /> 
+    <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+</Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
